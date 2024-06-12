130e4932a9e895b7a3b6e4c852500dd23c0d33e0
Frankie Milner <frankiemilner@frankies-mbp.mynetworksettings.com>
2024-06-12 13:54:22 -0400

    added css styling

--- puppy_pals/src/App.jsx
+++ puppy_pals/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
         );
       })}
       {featPupId && (
-      <div>
+      <div className="puppyCard">
       <h2>{featuredPup.name}</h2>
           <ul>
             <li>Age: {featuredPup.age}</li>
